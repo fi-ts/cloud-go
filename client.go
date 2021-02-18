cd84119779fc2bc6ebbad71924403422844fde67
thheinel <74293094+thheinel@users.noreply.github.com>
2021-02-18 11:52:30 +0100

    Closes #35 add timeout to client (#36)

--- client.go
+++ client.go
@@ -2,6 +2,7 @@ package cloudgo
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 	"time"
 
@@ -26,6 +27,7 @@ type clientSpec struct {
 	hmac  string
 
 	hmacAuthType string
+	timeout      time.Duration
 }
 
 type option func(c *clientSpec)
@@ -34,6 +36,13 @@ type option func(c *clientSpec)
 func AuthType(authType string) option {
 	return func(c *clientSpec) {
 		c.hmacAuthType = authType
+	}
+}
+
+//Timeout sets the timeout for a new client
+func Timeout(timeout time.Duration) option {
+	return func(c *clientSpec) {
+		c.timeout = timeout
 	}
 }
 
@@ -54,6 +63,7 @@ func NewClient(rawurl, token, hmac string, options ...option) (*client.CloudAPI,
 		token:        token,
 		hmac:         hmac,
 		hmacAuthType: defaultHMACAuthType,
+		timeout:      30 * time.Second,
 	}
 
 	for _, opt := range options {
@@ -83,7 +93,12 @@ func newClientTransport(c *clientSpec) *httptransport.Runtime {
 		return nil
 	})
 
-	transport := httptransport.New(c.host, c.basePath, c.schemes)
+	client := &http.Client{
+		Timeout:   c.timeout,
+		Transport: http.DefaultTransport,
+	}
+
+	transport := httptransport.NewWithClient(c.host, c.basePath, c.schemes, client)
 	transport.DefaultAuthentication = auther
 
 	return transport
