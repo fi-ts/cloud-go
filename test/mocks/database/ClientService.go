4dab79f37e0e5727b1f09d8c3dda343dee99badb
Philipp Eberle <eberlep@gmail.com>
2022-06-28 10:54:10 +0200

    Update cloud-api.json

--- test/mocks/database/ClientService.go
+++ test/mocks/database/ClientService.go
@@ -46,6 +46,36 @@ func (_m *ClientService) AcceptPostgresRestore(params *clientdatabase.AcceptPost
 	return r0, r1
 }
 
+// AcceptPostgresRestoreDeprecated provides a mock function with given fields: params, authInfo, opts
+func (_m *ClientService) AcceptPostgresRestoreDeprecated(params *clientdatabase.AcceptPostgresRestoreDeprecatedParams, authInfo runtime.ClientAuthInfoWriter, opts ...clientdatabase.ClientOption) (*clientdatabase.AcceptPostgresRestoreDeprecatedOK, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, params, authInfo)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *clientdatabase.AcceptPostgresRestoreDeprecatedOK
+	if rf, ok := ret.Get(0).(func(*clientdatabase.AcceptPostgresRestoreDeprecatedParams, runtime.ClientAuthInfoWriter, ...clientdatabase.ClientOption) *clientdatabase.AcceptPostgresRestoreDeprecatedOK); ok {
+		r0 = rf(params, authInfo, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*clientdatabase.AcceptPostgresRestoreDeprecatedOK)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*clientdatabase.AcceptPostgresRestoreDeprecatedParams, runtime.ClientAuthInfoWriter, ...clientdatabase.ClientOption) error); ok {
+		r1 = rf(params, authInfo, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // CreatePostgres provides a mock function with given fields: params, authInfo, opts
 func (_m *ClientService) CreatePostgres(params *clientdatabase.CreatePostgresParams, authInfo runtime.ClientAuthInfoWriter, opts ...clientdatabase.ClientOption) (*clientdatabase.CreatePostgresCreated, error) {
 	_va := make([]interface{}, len(opts))
