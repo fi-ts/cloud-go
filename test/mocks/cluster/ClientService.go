8a3490c344e54554d56fedd3de905009b8e3dddc
Gerrit <Gerrit91@users.noreply.github.com>
2022-08-22 15:58:44 +0200

    Monitoring secret endpoint. (#77)

--- test/mocks/cluster/ClientService.go
+++ test/mocks/cluster/ClientService.go
@@ -196,6 +196,36 @@ func (_m *ClientService) GetClusterKubeconfigTpl(params *clientcluster.GetCluste
 	return r0, r1
 }
 
+// GetMonitoringSecret provides a mock function with given fields: params, authInfo, opts
+func (_m *ClientService) GetMonitoringSecret(params *clientcluster.GetMonitoringSecretParams, authInfo runtime.ClientAuthInfoWriter, opts ...clientcluster.ClientOption) (*clientcluster.GetMonitoringSecretOK, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, params, authInfo)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *clientcluster.GetMonitoringSecretOK
+	if rf, ok := ret.Get(0).(func(*clientcluster.GetMonitoringSecretParams, runtime.ClientAuthInfoWriter, ...clientcluster.ClientOption) *clientcluster.GetMonitoringSecretOK); ok {
+		r0 = rf(params, authInfo, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*clientcluster.GetMonitoringSecretOK)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*clientcluster.GetMonitoringSecretParams, runtime.ClientAuthInfoWriter, ...clientcluster.ClientOption) error); ok {
+		r1 = rf(params, authInfo, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetSSHKeyPair provides a mock function with given fields: params, authInfo, opts
 func (_m *ClientService) GetSSHKeyPair(params *clientcluster.GetSSHKeyPairParams, authInfo runtime.ClientAuthInfoWriter, opts ...clientcluster.ClientOption) (*clientcluster.GetSSHKeyPairOK, error) {
 	_va := make([]interface{}, len(opts))
